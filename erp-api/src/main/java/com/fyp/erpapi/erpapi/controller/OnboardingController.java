c023455eb215f70fc64965281cffccd4839b3986
Mohamed Yusuf <mohdgazyanii235@gmail.com>
2023-11-21 03:08:15 +0000

    Added some comments to OnboardingController as some notes for that class

--- erp-api/src/main/java/com/fyp/erpapi/erpapi/controller/OnboardingController.java
+++ erp-api/src/main/java/com/fyp/erpapi/erpapi/controller/OnboardingController.java
@@ -8,4 +8,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/onboarding")
 public class OnboardingController {
 
+    /*
+    Following endpoints needed:
+    1) /onboarding/{userId}/userInfo (POST/PUT) giving users the ability to updated things like firstname, lastname, etc.
+    2) /onboarding/{userId}/companyInfo (POST/PUT) giving users the ability to join company and set their position - some functionality of approval to be added here.
+    3) /onboarding/{userId}/profileInfo (POST/PUT) giving users the ability to set their profile picture, etc.
+     */
+
 }
